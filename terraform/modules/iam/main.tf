0b5d6a2c855fca6467f4ed634d877c390520a117
Aayushsingh2001 <as6994770@gmail.com>
2025-07-03 21:19:13 +0530

    Fixing the end to end flow and having env variables fetched from AWS SSM

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -48,6 +48,28 @@ resource "aws_iam_role_policy" "ecr_access" {
   })
 }
 
+resource "aws_iam_role_policy" "ssm_access" {
+  name = "${var.environment}-ssm-access"
+  role = aws_iam_role.ec2_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+        {
+            Effect = "Allow"
+            Action = [
+                "ssm:GetParameter",
+                "ssm:GetParameters",
+                "ssm:GetParametersByPath"
+            ]
+            Resource = [
+              "arn:aws:ssm:${var.aws_region}:${var.aws_account_id}:parameter/${var.environment}/backend/*"
+            ]
+        }
+    ]
+  })
+}
+
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "${var.environment}-ec2-ecr-profile"
   role = aws_iam_role.ec2_role.name
