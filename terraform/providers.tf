fb50f5cc6d4a9b6526b726bdc117e0ec536b37f2
Aayushsingh2001 <as6994770@gmail.com>
2025-07-09 11:54:37 +0530

    Fixing the end to end flow and having env variables fetched from AWS SSM

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "aayush-terraform-state-bucket"
+    bucket = "aayu-terraform-state-bucket"
     key = "terraform.tfstate"
     region = "ap-south-1"
   }
